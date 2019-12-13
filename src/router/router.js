a99e244ceef791ad330351f4c044fa3715252739
JiuWoShen <2092317140@qq.com>
2019-12-13 11:31:09 +0800

    项目起步-整合路由与elementUI

---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+// 导入
+import VueRouter from 'vue-router'
+// 注册
+Vue.use(VueRouter)
+// 实例化
+const router=new VueRouter({
+    routes: [
+
+    ]
+})
+// 暴露
+export default router
+
